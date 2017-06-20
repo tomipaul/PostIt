9a736d1fb75211d6a28b3e16073acac61e02a329
tomi <tomipaul95@gmail.com>
2017-06-20 17:24:05 +0100

    [feat]: Define User model
    User model represents User database table
    In the application's context, its instance is a user who
    belongs to groups, post messages and read messages.

---
+++ server/models/user.js
@@ -0,0 +1,129 @@
+import bcrypt from 'bcrypt';
+
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: {
+        args: true,
+        msg: 'username is not available!'
+      },
+      validate: {
+        notEmpty: {
+          msg: 'username cannot be an empty string'
+        },
+        isAlphanumeric: {
+          msg: 'username can only contain letters and numbers'
+        },
+        len: {
+          args: [3, 25],
+          msg: 'username cannot be longer than 25 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: 'email is invalid',
+        },
+        len: {
+          args: [5, 254],
+          msg: 'email has invalid length'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6, 255],
+          msg: 'password must be at least six characters long'
+        }
+      }
+    },
+    phoneNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'mobile number cannot be an empty string'
+        },
+        isNumeric: {
+          msg: 'mobile number is invalid'
+        }
+      }
+    }
+  }, {
+    classMethods: {
+      associate(models) {
+        User.belongsToMany(models.Message, {
+          through: 'UserUnreadMessages'
+        });
+        User.belongsToMany(models.Group, {
+          through: 'GroupUsers'
+        });
+      }
+    },
+    instanceMethods: {
+      verifyPassword(password) {
+        return bcrypt.compare(password, this.password)
+        .then((res) => {
+          return res;
+        })
+        .catch((err) => {
+          return err;
+        });
+      }
+    },
+    hooks: {
+      beforeCreate(user) {
+        if (user.isNewRecord) {
+          return bcrypt.hash(user.password, 10)
+          .then((hash) => {
+            user.password = hash;
+            return user.password;
+          })
+          .catch((err) => {
+            return err;
+          });
+        }
+      }
+    },
+    beforeUpdate(user) {
+      if (user.changed('password')) {
+        return bcrypt.hash(user.password, 10)
+        .then((hash) => {
+          user.password = hash;
+          return user.password;
+        })
+        .catch((err) => {
+          return err;
+        });
+      }
+    }
+  });
+  User.associate = function associate(models) {
+    User.belongsToMany(models.Message, {
+      through: 'UserUnreadMessages'
+    });
+    User.belongsToMany(models.Group, {
+      through: 'GroupUsers'
+    });
+  };
+  User.prototype.verifyPassword = function verifyPassword(password) {
+    return bcrypt.compare(password, this.password)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      throw err;
+    });
+  };
+  return User;
+};
