705e2d1cbfff8800d25719ce9a9c3e1ba17f3ed2
tomi <tomipaul95@gmail.com>
2017-06-20 20:53:16 +0100

    [chore]: Add database config/credentials

---
+++ server/config/config.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { URL, USERNAME, PASSWORD, DATABASE_NAME, HOST } = process.env;
+export default {
+  development: {
+    username: 'postgres',
+    password: 'admin',
+    database: 'PostIt',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'postgres',
+    password: null,
+    database: 'PostIt_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    database_url: URL,
+    username: USERNAME,
+    password: PASSWORD,
+    database: DATABASE_NAME,
+    host: HOST,
+    dialect: 'postgres'
+  }
+};
