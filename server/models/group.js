66fb174f3c5a508f2d6a75a358275639f6bed25c
tomi <tomipaul95@gmail.com>
2017-06-20 17:17:30 +0100

    [feat]: Define Group model
    Group model represents Group database table
    In the application's context, its instance is a notification group
    to which users are added and messages are posted.

---
+++ server/models/group.js
@@ -0,0 +1,37 @@
+export default (sequelize, DataTypes) => {
+  const Group = sequelize.define('Group', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+      validate: {
+        isUUID: {
+          args: 4,
+          msg: 'id must be uuid'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Group name cannot be an empty string'
+        },
+        is: {
+          args: /^[a-z1-9_]+$/i,
+          msg: 'Name can contain only letters, numbers and underscores'
+        }
+      }
+    },
+    description: {
+      type: DataTypes.STRING
+    }
+  });
+  Group.associate = function associate(models) {
+    Group.hasMany(models.Message);
+    Group.belongsToMany(models.User, { through: 'GroupUsers' });
+  };
+  return Group;
+};
