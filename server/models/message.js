603014f24862a94fe65a4a8bf0bbd2fbf30ebdb0
tomi <tomipaul95@gmail.com>
2017-06-20 17:26:55 +0100

    [feat]: Define Message model
    Message model represents Message database table
    In the application's context, its instance is a message
    posted to group(s) to be viewed by users.

---
+++ server/models/message.js
@@ -0,0 +1,45 @@
+export default (sequelize, DataTypes) => {
+  const Message = sequelize.define('Message', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+      validate: {
+        isUUID: {
+          args: 4,
+          msg: 'id must be uuid'
+        }
+      }
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Message cannot be empty'
+        },
+      }
+    },
+    priority: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [['normal', 'urgent', 'critical']],
+          msg: 'priority is invalid'
+        }
+      }
+    },
+  });
+  Message.associate = function associate(models) {
+    Message.belongsTo(models.User, {
+      as: 'Author',
+      onDelete: 'SET NULL'
+    });
+    Message.belongsToMany(models.User, {
+      through: 'UserUnreadMessages'
+    });
+  };
+  return Message;
+};
