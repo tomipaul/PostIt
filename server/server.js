b0cb6c8353fb9fceeb9ea34c26ea641316e5fbf5
tomi <tomipaul95@gmail.com>
2017-06-21 02:40:26 +0100

    [chore]: Parse request cookies

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import http from 'http';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import groupRouter from './routes/GroupRoutes';
 import userRouter from './routes/UserRoutes';
 
 const app = express();
+app.use(cookieParser());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(userRouter);
