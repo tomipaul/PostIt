71bb9551a45679713840e29d98a2b4ff86bc2b38
tomi <tomipaul95@gmail.com>
2017-06-22 05:23:58 +0100

    [chore]: Add seeders for database

---
+++ server/seeders/20170622015048-group-seeder.js
@@ -0,0 +1,43 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Groups', [{
+      id: 'cc4022a6-7b42-45cb-ac34-0076a22032d3',
+      name: 'FirstGroup',
+      description: 'The beginning',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      id: '6c8b1858-604a-44f6-a37c-0cbd678b2800',
+      name: 'FidelityGroup',
+      description: 'We are a group, we keep our word',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      id: '8a317435-f509-4cc2-9ebd-1a2190a30670',
+      name: 'AccessGroup',
+      description: 'Happy people, Happy Group',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      id: '5223abfd-97ef-4bbf-ba67-a351e3e09faf',
+      name: 'ZenithGroup',
+      description: 'In your best interest, join our group',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      id: '46b2e248-43a0-4868-ab94-7b5928c7f3f9',
+      name: 'GTGroup',
+      description: "Wouldn't you rather join our group?",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Groups', null, {});
+  }
+};
---
+++ server/seeders/20170622015048-user-seeder.js
@@ -0,0 +1,60 @@
+import bcrypt from 'bcrypt';
+
+const passwords = [
+  'blockers',
+  'shockers',
+  'lockers',
+  'dockers',
+  'mockers'
+];
+const hashedPasswords = passwords.map((password) => {
+  return bcrypt.hashSync(password, 10);
+});
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      username: 'Tomi Paul',
+      email: 'tomipaul95@gmail.com',
+      password: hashedPasswords[0],
+      phoneNo: '2348107976596',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      username: 'Yaya Mark',
+      email: 'yayamark@gmail.com',
+      password: hashedPasswords[1],
+      phoneNo: '2348107976597',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      username: 'Makum Kaji',
+      email: 'makumkaji@gmail.com',
+      password: hashedPasswords[2],
+      phoneNo: '2348107976598',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      username: 'Phil Bone',
+      email: 'philbone@gmail.com',
+      password: hashedPasswords[3],
+      phoneNo: '2348107976599',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      username: 'Van Ejim',
+      email: 'vanejim@gmail.com',
+      password: hashedPasswords[4],
+      phoneNo: '2348107976600',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
