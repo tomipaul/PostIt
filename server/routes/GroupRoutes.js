646930a42e1c8183432ccf3dcace2d668c7305ff
tomi <tomipaul95@gmail.com>
2017-06-29 05:49:13 +0100

    [feat]: Mount authorization middlewares on endpoints

--- server/routes/GroupRoutes.js
+++ server/routes/GroupRoutes.js
@@ -4,10 +4,13 @@ import GroupController from '../controllers/GroupController';
 const router = express.Router();
 router.post('/api/group', GroupController.createGroup());
 router.post('/api/group/:groupId/user',
+ GroupController.permitOnlyGroupMembers(),
  GroupController.addUserToGroup());
 router.post('/api/group/:groupId/message',
+ GroupController.permitOnlyGroupMembers(),
  GroupController.addMessageToGroup());
 router.get('/api/group/:groupId/messages',
+ GroupController.permitOnlyGroupMembers(),
  GroupController.getGroupMessages());
 
 export default router;
