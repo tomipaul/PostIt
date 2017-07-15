ec01e0a8a5708c3cc477d95e4bee6e23a31c981a
Tomi Paul <tomipaul95@gmail.com>
2017-07-15 12:10:03 +0100

    [refactor] remove sequelize sync; use migrations

--- server/index.js
+++ server/index.js
@@ -1,25 +1,14 @@
 import dotenv from 'dotenv';
 import './CreateRSAKeyFile';
-import models from './models';
 import app from './server';
 
 dotenv.config();
 dotenv.config({ path: `${__dirname}/key/rsapair.pem` });
-const env = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 8080;
-if (env === 'development') {
-  models.sequelize.sync({ force: true })
-  .then(() => {
-    app.listen(port, () => {
-      //eslint-disable-next-line
-      console.log(`Server started on ${port}`);
-    });
-  });
-} else {
-  app.listen(port, () => {
-    //eslint-disable-next-line
-    console.log(`Server started on ${port}`);
-  });
-}
+
+app.listen(port, () => {
+  //eslint-disable-next-line
+  console.log(`Server started on ${port}`);
+});
 
 export default app;
