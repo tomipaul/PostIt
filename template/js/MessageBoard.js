27ee213b6139b9b1b36e0f50b60618cfaafeb7b6
Tomi Paul <tomipaul95@gmail.com>
2017-07-28 02:14:26 +0100

    [feat] update templates for front-end

--- template/js/MessageBoard.js
+++ template/js/MessageBoard.js
@@ -1,5 +1,4 @@
 // Initialize collapse button
-
 $(document).ready(() => {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $(".button-collapse").sideNav();
@@ -13,5 +12,16 @@ $(document).ready(() => {
     const converter = new showdown.Converter();
     const html = converter.makeHtml(e.target.value);
     e.innerHTML = html;
+  });
+  $('.label').click((e) => {
+    $('.label').removeAttr('priority');
+    const message = e.target;
+    if (message.textContent === 'normal') {
+      message.setAttribute('priority', 'normal');
+    } else if (message.textContent === 'urgent') {
+      message.setAttribute('priority', 'urgent');
+    } else if (message.textContent === 'critical') {
+      message.setAttribute('priority', 'critical');
+    }
   });
 });
