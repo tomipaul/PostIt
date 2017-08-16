2460bd78c42c5186460bc943d701840d370ebe76
Tomi Paul <tomipaul95@gmail.com>
2017-08-17 00:15:00 +0100

    [feat] create and configure store
    create store from combined reducers and apply middlwares for logging and asynchronous actions

---
+++ client/store/store.js
@@ -0,0 +1,18 @@
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
+
+const loggerMiddleware = (process.env.NODE_ENV === 'development') ?
+createLogger() : null;
+const configureStore = (preloadedState) => {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware
+    )
+  );
+};
+export default configureStore;
