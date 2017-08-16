1ba6cb01871b9ce5e8b0aed588169fe8a89064e1
Tomi Paul <tomipaul95@gmail.com>
2017-08-17 00:06:32 +0100

    [feat] add entry point for application

---
+++ client/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'materialize-css/dist/js/materialize.js';
+import 'materialize-css/dist/css/materialize.min.css';
+import './bin/css/signup-login.css';
+import './bin/css/board.css';
+import Root from './components/container/Root';
+
+const config = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
+};
+firebase.initializeApp(config);
+render(
+  <Root />,
+  document.getElementById('root')
+);
