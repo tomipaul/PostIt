87f933d561645d4eb8c5823845b30f0864b7fa2c
Tomi Paul <tomipaul95@gmail.com>
2017-08-17 05:26:02 +0100

    [feat] declare asset folder and create route that serves index page

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
@@ -8,12 +9,16 @@ import userRouter from './routes/UserRoutes';
 import errorHandler from './controllers/ErrorHandler';
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, '../client/bin')));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(authRouter);
 app.use(userRouter);
 app.use(groupRouter);
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/bin/index.html'));
+});
 app.use(errorHandler());
 
 const server = http.createServer(app);
