e66ace0febe8929372839a7410d9884bb37a797e
Tomi Paul <tomipaul95@gmail.com>
2017-08-21 11:09:00 +0100

    [feat] implement markdown
    user can write message using the markdown syntax

--- client/components/presentation/Main/MessageBoard/Message.jsx
+++ client/components/presentation/Main/MessageBoard/Message.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Message = ({ Author, priority, text }) =>
-  (
+/* eslint-disable */
+const Message = ({ Author, priority, text }) => {
+  const matched = text.split(/<\/?p>/);
+  return (
     <div className="message-text">
       <a href=""><span className="author-name">{Author}</span></a>
       <div className="priority-tag">
         <span>{priority}</span>
         <i className="fa fa-tags" aria-hidden="true" />
       </div>
-      <div
-        className="message"
-      >{text}
-      </div>
+      {
+        (matched.length > 1) ?
+        (
+          <div
+            dangerouslySetInnerHTML={{ __html: matched[1]}}
+            className="message"
+          />
+        ) : (
+          <div
+            dangerouslySetInnerHTML={{ __html: matched[0]}}
+            className="message"
+          />
+        )
+      }
     </div>
   );
+}
 
 Message.propTypes = {
   text: PropTypes.string.isRequired,
