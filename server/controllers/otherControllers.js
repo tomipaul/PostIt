dcaa30bb8451df64045cc0198ecbcbcc563f4bba
Tomi Paul <tomipaul95@gmail.com>
2017-09-05 06:08:27 +0100

    [fix] exclude message author from receiving notification
    When messages are posted to group, notifications are sent to memebers of the group
    Before the author of the message also receives the notification
    Now, the author of the message is excluded when sending notifications

--- server/controllers/otherControllers.js
+++ server/controllers/otherControllers.js
@@ -3,10 +3,15 @@ import NotificationService from '../services/NotificationService.js';
 const Notification = new NotificationService();
 
 export const sendNotifications = (req, res, next) => {
-  req.group.getUsers()
+  const createdMessage = req.res.data.createdMessage;
+  req.group.getUsers({
+    where: {
+      username: { ne: createdMessage.AuthorUsername }
+    }
+  })
   .then((users) => {
     users.forEach((user) => {
-      const messsagePriority = req.res.data.createdMessage.priority;
+      const messsagePriority = createdMessage.priority;
       if (messsagePriority === 'urgent') {
         Notification.sendMailNotification(user, req.group);
       } else if (messsagePriority === 'critical') {
