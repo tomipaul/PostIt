0ab7f693f5bc79e575dd75e60bfd04f76a4d0e6e
Tomi Paul <tomipaul95@gmail.com>
2017-09-21 12:21:20 +0100

    [fix]: add navigation buttons to 404 error page
    Add a button to the 404 error page for the user to navigate back to the authentication screen or dashboard
    if the user is authenticated, dashboard
    if not, the authentication screen
    [Delivers #151285285]

--- client/components/presentation/Common/NotFound.jsx
+++ client/components/presentation/Common/NotFound.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 /**
  * component displayed when user navigates to a
@@ -6,9 +8,27 @@ import React from 'react';
  * @function NotFound
  * @returns {object} NotFound component
  */
-const NotFound = () =>
-  (
-    <div className="error-404" />
+export const NotFound = ({ isAuthenticated }) => {
+  const buttonText = (isAuthenticated) ?
+  'Back to Dashboard' : 'Back to Homepage';
+  const location = (isAuthenticated) ?
+  '/dashboard' : '/';
+  return (
+    <div className="error-404">
+      <a
+        className="waves-effect waves-light btn"
+        href={location}
+      >{buttonText}</a>
+    </div>
   );
+};
 
-export default NotFound;
+NotFound.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(NotFound);
