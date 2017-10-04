aedc35b5cadd26418b7473440e2466cf9d23679e
Tomi Paul <tomipaul95@gmail.com>
2017-10-04 04:32:11 +0100

    [fix]: remove console.log from server file
    use winston logger to log to file

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 import dotenv from 'dotenv';
+import winston from 'winston';
 import './CreateRSAKeyFile';
 import app from './server';
 
 dotenv.config();
 dotenv.config({ path: `${__dirname}/key/rsapair.pem` });
 const port = process.env.PORT || 8080;
+winston.configure({
+  transports: [
+    new (winston.transports.File)({ filename: 'server.log' })
+  ]
+});
 
 app.listen(port, () => {
-  //eslint-disable-next-line
-  console.log(`Server started on ${port}`);
+  winston.log('info', `Server started on ${port}`);
 });
 
 export default app;
